2315c511d47e0c9ed6fabad1b87086638b1055a9
donghaiwang <whd@hutb.edu.cn>
2025-06-13 14:33:46 +0800

    测试清除debug字符串

--- PythonAPI/test/unit/test_clear_debug_shape.py
+++ PythonAPI/test/unit/test_clear_debug_shape.py
@@ -31,9 +31,11 @@ world_snapshot = world.get_snapshot()
 for actor_snapshot in world_snapshot:
     actual_actor = world.get_actor(actor_snapshot.id)
     if actual_actor.type_id == 'traffic.traffic_light':
-        debug.draw_box(carla.BoundingBox(actor_snapshot.get_transform().location,carla.Vector3D(0.5,0.5,2)),actor_snapshot.get_transform().rotation, 0.05, carla.Color(255,0,0,0),0)
+        debug.draw_box(carla.BoundingBox(actor_snapshot.get_transform().location,carla.Vector3D(0.5,0.5,2)), actor_snapshot.get_transform().rotation, 0.05, carla.Color(255, 0, 0, 0), 0)
 # ...
 
-time.sleep(3)
+print("start clear string")
 # 清除调式时的绘制信息
-debug_helper.clear_debug_shape()
+debug_helper.clear_debug_string()
+time.sleep(3)
+print("clear finished")
