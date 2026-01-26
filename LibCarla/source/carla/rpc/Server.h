6aef8ba16acf39b62c9e5c10a0556ef9e0308907
donghaiwang <whd@hutb.edu.cn>
2026-01-26 23:25:53 +0800

    [carla] Extend ROS2 support Step 2: Fine grained ServerSynchronization (#9450) * Extend ROS2 support Step 2: Fine grained ServerSynchronization
    Introduced a fine grained ServerSynchronization mechanism, where each
    synchonization participant is treated independently and interacts
    with the synchronization of the carla-server individually. If a client
    is disconnected (or dies) the synchronization state of all participants
    registered via that client are dropped, i.e. the server will continue
    running in case the participants of that client were the only ones
    demanding synchronous mode.
    
    The synchronization interface provides means of a time window, up to
    which the server is allowed to run. Like this, every client can prevent
    the carla-server to run too fast depending on their individual speed.
    There is no sync-master anymore. Every client decides for its own if
    it requires synchronization or not.
    
    Drawback of this change: some existing code might have to be changed
    (see removal of synchronous_master in generate_traffic.py).
    
    * Fix Windows build and smoke tests
    
    Update RPC lib version for windows build
    
    Ensure tick calls are ignored if sync mode is not active.
    And prevent client changes of fixed_delta_seconds triggering warning
    message.
    
    * Client needs to wait for next tick
    
    on non synchronous mode
    
    This is one of the second steps of the ROS2 extension series.
    This one requires #9431 to be merged; until then it's listed as a draft.
    
    Introduced a fine grained ServerSynchronization mechanism, where each
    synchonization participant is treated independently and interacts
    with the synchronization of the carla-server individually. If a client
    is disconnected (or dies) the synchronization state of all participants
    registered via that client are dropped, i.e. the server will continue
    running in case the participants of that client were the only ones
    demanding synchronous mode.
    
    The synchronization interface provides means of a time window, up to
    which the server is allowed to run. Like this, every client can prevent
    the carla-server to run too fast depending on their individual speed.
    There is no sync-master anymore. Every client decides for its own if
    it requires synchronization or not.
    
    Drawback of this change: some existing code might have to be changed
    (see removal of synchronous_master in generate_traffic.py).
    
    # Conflicts:
    #	LibCarla/source/carla/rpc/Server.h

--- LibCarla/source/carla/rpc/Server.h
+++ LibCarla/source/carla/rpc/Server.h
@@ -15,6 +15,7 @@
 #include <boost/asio/post.hpp>
 
 #include <rpc/server.h>
+#include <rpc/this_session.h>
 
 #include <atomic>
 #include <future>
@@ -67,6 +68,14 @@ namespace rpc {
       _server.stop();
     }
 
+    void BindOnClientConnected(::rpc::server::callback_type callback) {
+      _server.set_on_connection(callback);
+    }
+
+    void BindOnClientDisconnected(::rpc::server::callback_type callback) {
+      _server.set_on_disconnection(callback);
+    }
+
   private:
 
     boost::asio::io_context _sync_io_context;
@@ -111,7 +120,9 @@ namespace detail {
     template <typename FuncT>
         static auto WrapSyncCall(std::atomic_bool &shutdown_in_progress, boost::asio::io_context &io, FuncT &&functor) {
       return [&shutdown_in_progress, &io, functor=std::forward<FuncT>(functor)](Metadata metadata, Args... args) -> R {
-        auto task = std::packaged_task<R()>([functor=std::move(functor), args...]() {
+        auto const session_id = ::rpc::this_session().id();
+        auto task = std::packaged_task<R()>([session_id, functor=std::move(functor), args...]() {
+          ::rpc::this_session().set_id(session_id);
           return functor(args...);
         });
         if (metadata.IsResponseIgnored()) {
