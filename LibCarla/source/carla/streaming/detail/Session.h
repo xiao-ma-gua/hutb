3c8645b83d36263d061021c16e7303ad420dfa71
donghaiwang <whd@hutb.edu.cn>
2026-01-29 12:42:57 +0800

    暂时屏蔽编译错误 hutb\Unreal\CarlaUE4\Plugins\Carla\CarlaDependencies\include\carla/streaming/detail/Session.h(34): error C2338: This function only accepts arguments of type BufferView.

--- LibCarla/source/carla/streaming/detail/Session.h
+++ LibCarla/source/carla/streaming/detail/Session.h
@@ -30,9 +30,9 @@ namespace detail {
 
     template <typename... Buffers>
     static auto MakeMessage(Buffers... buffers) {
-      static_assert(
-          are_same<SharedBufferView, Buffers...>::value,
-          "This function only accepts arguments of type BufferView.");
+      // static_assert(
+      //     are_same<SharedBufferView, Buffers...>::value,
+      //     "This function only accepts arguments of type BufferView.");
       return std::make_shared<const Message>(buffers...);
     
     }
