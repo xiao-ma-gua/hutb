98e7dc1a1c84e53a4c8d140552ae43901390022c
jiandaoshou-aidehua <2893666867@qq.com>
2026-06-10 21:35:32 +0800

    解决LFS上传不了的问题

--- Util/BuildTools/Linux.mk
+++ Util/BuildTools/Linux.mk
@@ -80,7 +80,8 @@ examples:
 run-examples:
 	@for D in ${CARLA_EXAMPLES_FOLDER}/*; do [ -d "$${D}" ] && make -C $${D} run.only; done
 
-CarlaUE4Editor: LibCarla.server.release osm2odr downloadplugins
+# CarlaUE4Editor: LibCarla.server.release osm2odr downloadplugins
+CarlaUE4Editor:
 	@${CARLA_BUILD_TOOLS_FOLDER}/BuildUE4Plugins.sh --build $(ARGS)
 	@${CARLA_BUILD_TOOLS_FOLDER}/BuildCarlaUE4.sh --build $(ARGS)
 
