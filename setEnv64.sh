fa2a05e163d6a27ca8cc53aad41e48c03b6f1a83
xiao-ma-gua <17358869549@163.com>
2026-08-04 11:53:21 +0800

    打开设置的代理

--- setEnv64.sh
+++ setEnv64.sh
@@ -1,5 +1,5 @@
-# set http_proxy=http://127.0.0.1:7890
-# set https_proxy=http://127.0.0.1:7890
+set http_proxy=http://127.0.0.1:7890
+set https_proxy=http://127.0.0.1:7890
 
 # python.exe
 set PATH=/usr/bin/python 
